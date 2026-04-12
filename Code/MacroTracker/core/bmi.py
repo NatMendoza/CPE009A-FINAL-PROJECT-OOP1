693858ede1e25757134933206f18ea19e9a99668
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-12 21:40:08 +0800

    Update bmi.py
    Slight changes

--- Code/MacroTracker/core/bmi.py
+++ Code/MacroTracker/core/bmi.py
@@ -1,13 +1,4 @@
 class BMI:
     @staticmethod
-    def compute(weight, height):
-        return weight / (height ** 2)
-
-    @staticmethod
-    def advice(bmi):
-        if bmi < 18.5:
-            return "Underweight"
-        elif bmi < 25:
-            return "Normal"
-        else:
-            return "Overweight"
+    def compute(w, h):
+        return w / (h*h)
