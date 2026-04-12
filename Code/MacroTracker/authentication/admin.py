d528e47b09909a5f9fc6303403c185cc5b1e183f
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-12 21:41:44 +0800

    Update admin.py

--- Code/MacroTracker/authentication/admin.py
+++ Code/MacroTracker/authentication/admin.py
@@ -1,9 +1,4 @@
-from models.person import Person
-
-class Admin(Person):
-    def __init__(self, username, password, admin_id):
-        super().__init__(username, password)
-        self.__admin_id = admin_id
-
-    def verify_admin(self, admin_id):
-        return self.__admin_id == admin_id
+class Admin:
+    def __init__(self, user):
+        self.user = user
+        self.role = "admin"
