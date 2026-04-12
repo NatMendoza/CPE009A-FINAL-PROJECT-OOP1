cdf3d12437d31999283908800b87f3387b4660ae
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-12 21:41:27 +0800

    Update system.py

--- Code/MacroTracker/authentication/system.py
+++ Code/MacroTracker/authentication/system.py
@@ -1,45 +1,23 @@
 from data.database import Database
-from rich import print
 
 db = Database()
 
 class AuthSystem:
 
     def register(self):
-        username = input("Username: ")
-        password = input("Password: ")
+        u = input("User: ")
+        p = input("Pass: ")
 
-        if db.create_user(username, password):
-            print("[green]User registered![/green]")
-        else:
-            print("[red]User already exists[/red]")
+        data = db.load()
+        data[u] = p
+        db.save(data)
 
     def login(self):
-        username = input("Username: ")
-        password = input("Password: ")
+        u = input("User: ")
+        p = input("Pass: ")
 
-        stored = db.get_user(username)
+        data = db.load()
 
-        if stored and stored == password:
-            print("[green]Login successful![/green]")
-            return username
-        else:
-            print("[red]Invalid credentials[/red]")
-            return None
-
-    def update(self):
-        username = input("Username: ")
-        new_pass = input("New Password: ")
-
-        if db.update_user(username, new_pass):
-            print("[yellow]Updated successfully[/yellow]")
-        else:
-            print("[red]User not found[/red]")
-
-    def delete(self):
-        username = input("Username: ")
-
-        if db.delete_user(username):
-            print("[red]User deleted[/red]")
-        else:
-            print("[red]User not found[/red]")
+        if u in data and data[u] == p:
+            print("Login success")
+            return True
