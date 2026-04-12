7fa221fe30f1059b511e13a156bc60cc9d303ec5
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-12 16:10:23 +0800

    Update admin.py

--- Code/MacroTracker/authentication/admin.py
+++ Code/MacroTracker/authentication/admin.py
@@ -1,14 +1,9 @@
-import random
 from models.person import Person
 
-class Admin(Person):   # INHERITANCE
-
-    def __init__(self, username, password):
+class Admin(Person):
+    def __init__(self, username, password, admin_id):
         super().__init__(username, password)
-        self.__admin_id = random.randint(1000, 999999)  # ENCAPSULATION
+        self.__admin_id = admin_id
 
-    def get_role(self):   # POLYMORPHISM
-        return "admin"
-
-    def get_admin_id(self):
-        return self.__admin_id
+    def verify_admin(self, admin_id):
+        return self.__admin_id == admin_id
