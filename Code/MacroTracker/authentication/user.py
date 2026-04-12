b68857f826008b27d222e9b46431fc85c99600dd
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-12 16:10:36 +0800

    Update user.py

--- Code/MacroTracker/authentication/user.py
+++ Code/MacroTracker/authentication/user.py
@@ -1,9 +1,5 @@
 from models.person import Person
 
-class User(Person):   # INHERITANCE
-
+class User(Person):
     def __init__(self, username, password):
         super().__init__(username, password)
-
-    def get_role(self):   # POLYMORPHISM
-        return "user"
