805e5069750459997b4f2c1cb94b8adcd49ccc90
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-13 08:29:33 +0800

    Update admin.py
    I have finished the overall authentication system kindly check if its working correctly

--- Code/MacroTracker/authentication/admin.py
+++ Code/MacroTracker/authentication/admin.py
@@ -1,4 +1,14 @@
-class Admin:
-    def __init__(self, user):
-        self.user = user
-        self.role = "admin"
+import random
+from models.person import Person
+
+class Admin(Person):   # INHERITANCE
+
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.__admin_id = random.randint(1000, 999999)  # ENCAPSULATION
+
+    def get_role(self):   # POLYMORPHISM
+        return "admin"
+
+    def get_admin_id(self):
+        return self.__admin_id
