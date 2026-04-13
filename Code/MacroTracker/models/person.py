158722203d77c61ccd9d7f1c374caf04358fd38e
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-13 11:59:22 +0800

    Update person.py

--- Code/MacroTracker/models/person.py
+++ Code/MacroTracker/models/person.py
@@ -1,4 +1,14 @@
-class Person:
-    def __init__(self, u, p):
-        self.username = u
-        self.password = p
+from abc import ABC, abstractmethod
+
+class Person(ABC):   # ABSTRACTION
+
+    def __init__(self, username, password):
+        self._username = username   # ENCAPSULATION
+        self._password = password   # ENCAPSULATION
+
+    def get_username(self):
+        return self._username
+
+    @abstractmethod
+    def get_role(self):   # ABSTRACTION
+        pass
