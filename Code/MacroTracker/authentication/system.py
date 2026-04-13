2011114b3043bccb74ff6798539ec89f402fc3b8
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-13 18:46:48 +0800

    Update system.py

--- Code/MacroTracker/authentication/system.py
+++ Code/MacroTracker/authentication/system.py
@@ -4,10 +4,12 @@ from authentication.user import User
 from authentication.admin import Admin
 from rich import print
 
-db = Database()
 SECRET_CODE = "MacroTrackerAdmin"
 
 class AuthSystem:
+
+    def __init__(self):
+        self.db = Database()
 
     def log(self, message):
         with open("logs.txt", "a") as f:
@@ -16,10 +18,10 @@ class AuthSystem:
     def register_user(self):
         username = input("Username: ")
         password = input("Password: ")
-    
-        user = User(username, password)  # OBJECT
 
-        if db.create_user(username, password, user.get_role()):
+        user = User(username, password)
+
+        if self.db.create_user(username, password, user.get_role()):
             print("[green]User registered![/green]")
             self.log(f"User registered: {username}")
         else:
@@ -40,7 +42,7 @@ class AuthSystem:
         admin = Admin(username, password)
         admin_id = admin.get_admin_id()
 
-        if db.create_user(username, password, admin.get_role(), admin_id):
+        if self.db.create_user(username, password, admin.get_role(), admin_id):
             print(f"[green]Admin registered! Your ID: {admin_id}[/green]")
             self.log(f"Admin registered: {username}")
         else:
@@ -50,7 +52,7 @@ class AuthSystem:
         username = input("Username: ")
         password = input("Password: ")
 
-        user = db.get_user(username)
+        user = self.db.get_user(username)
 
         if not user or user["password"] != password:
             print("[red]Invalid credentials[/red]")
