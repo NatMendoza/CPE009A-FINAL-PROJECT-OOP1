b4b5f617b45313f3ee380b0d1d3fed198b16dc0f
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-15 07:27:30 +0800

    Update admin.py

--- Code/MacroTracker/authentication/admin.py
+++ Code/MacroTracker/authentication/admin.py
@@ -1,14 +1,14 @@
-import random
-from models.person import Person
+import random                                            # This module allows generation of random numbers.
+from models.person import Person                         # Imports the base class.
 
-class Admin(Person):   # INHERITANCE
+class Admin(Person):                                     # This performs INHERITANCE in application to OOP, which Admin inherits from person.
 
     def __init__(self, username, password):
         super().__init__(username, password)
-        self.__admin_id = random.randint(1000, 999999)  # ENCAPSULATION
+        self.__admin_id = random.randint(1000, 999999)   # ENCAPSULATION is applied, which generates a random admin identification ID under private sector.
 
-    def get_role(self):   # POLYMORPHISM
+    def get_role(self):                                  # POLYMORPHISM applied, basically a subclass from a parent class person.
         return "admin"
 
-    def get_admin_id(self):
+    def get_admin_id(self):                              # This provides access to the private admin ID.
         return self.__admin_id
