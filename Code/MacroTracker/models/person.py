c0fea72efd8826599aed3676bd0ac22b0171d767
NatMendoza <qnmendoza@tip.edu.ph>
2026-04-15 07:43:19 +0800

    Update person.py

--- Code/MacroTracker/models/person.py
+++ Code/MacroTracker/models/person.py
@@ -1,14 +1,14 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod            # Imported abstraction tools that will be used for this file.
 
-class Person(ABC):   # ABSTRACTION
+class Person(ABC):                             # ABSTRACTION used, this abstracts the base class.
 
     def __init__(self, username, password):
-        self._username = username   # ENCAPSULATION
-        self._password = password   # ENCAPSULATION
+        self._username = username              # ENCAPSULATION private sector, an attribute to be protected.
+        self._password = password              # ENCAPSULATION private sector, an attribute to be protected.
 
-    def get_username(self):
+    def get_username(self):                    # Getter method, wherein it gets the username as the new self.
         return self._username
 
     @abstractmethod
-    def get_role(self):   # ABSTRACTION
+    def get_role(self):                        # ABSTRACTION used, which force subclasses to implement this method.
         pass
