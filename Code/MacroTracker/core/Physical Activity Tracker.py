9d70a7d48ef2770d746e817710c4c13e5b0e9f86
Pilapilcpe <qljpilapil@tip.edu.ph>
2026-04-15 15:21:44 +0800

    Update Physical Activity Tracker.py

--- Code/MacroTracker/core/Physical Activity Tracker.py
+++ Code/MacroTracker/core/Physical Activity Tracker.py
@@ -1,1 +1,17 @@
+class PhysicalActivityTracker:
+    def __init__(self):
+        self.recommendations = {
+            "low": 30,
+            "moderate": 20,
+            "high": 10
+        }
 
+    def get_suggestion(self, intensity, time_minutes):
+        recommended_time = self.recommendations[intensity]
+
+        if time_minutes < recommended_time:
+            return "Add more intensity or time."
+        elif time_minutes > recommended_time * 1.5:
+            return "Take it a bit slower."
+        else:
+            return "Great job!"
